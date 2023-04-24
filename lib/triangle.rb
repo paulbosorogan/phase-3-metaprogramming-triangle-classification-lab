69027909212ca6018333b12063049f6b172d53e8
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-04-24 17:43:26 -0400

    commit tree 5d0383d2860bcaaecc88bacf9c33e62cc61baeab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a= a
+    @b= b
+    @c= c
+  end
+
+  def kind(a,b,c)
+    if a == b && b == c
+      :equilateral
+    if a == b || a == c
+      :isosceles
+    else 
+      begin
+        raise TriangleError
+        :scalene
+      end
+    end
+  end 
+
+  class TriangleError < StandardError
+  end
 end
